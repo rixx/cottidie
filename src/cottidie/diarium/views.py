6e1e713590ec71f41fc65daae25a9c0a3e775a4d
Tobias Kunze <rixx@cutebit.de>
2016-11-27 15:08:30 +0100

    Add views to diarium app

---
+++ src/cottidie/diarium/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class MainView(LoginRequiredMixin, TemplateView):
+    login_url = '/login/'
+    template_name = 'diarium/main.html'
--- src/cottidie/urls.py
+++ src/cottidie/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from .base import urls as baseurls
+from .diarium import urls as diariumurls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^diarium/', include(diariumurls, namespace='diarium')),
     url(r'', include(baseurls, namespace='base')),
 ]
