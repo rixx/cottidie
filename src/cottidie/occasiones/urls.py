745972a6a24c37058bda49fa2593d8367e1e38e2
Tobias Kunze <rixx@cutebit.de>
2016-12-02 15:00:22 +0100

    Add a page to add a new occasion type

--- src/cottidie/occasiones/urls.py
+++ src/cottidie/occasiones/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url('^occasio/(?P<pk>\d+)/new/$', views.OccasionCreateView.as_view(), name='occasion-create'),
     url('^occasio/(?P<pk>\d+)/$', views.OccasionView.as_view(), name='occasion-type'),
-    url('^occasio/new/$', views.OccasionTypeCreateView.as_view(), name='occasions-type-create'),
-    url('^occasio/$', views.OccasionTypeView.as_view(), name='occasions-type-list'),
+    url('^occasio/new/$', views.OccasionTypeCreateView.as_view(), name='occasion-type-create'),
+    url('^occasio/$', views.OccasionTypeView.as_view(), name='occasion-type-list'),
     url('^$', views.MainView.as_view(), name='main'),
 ]
--- src/cottidie/urls.py
+++ src/cottidie/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 
 from .base import urls as baseurls
 from .diarium import urls as diariumurls
+from .occasiones import urls as occasionesurls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^diarium/', include(diariumurls, namespace='diarium')),
+    url(r'^occasiones/', include(occasionesurls, namespace='occasiones')),
     url(r'', include(baseurls, namespace='base')),
 ]
