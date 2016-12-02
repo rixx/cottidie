aedf143309e5ab9899e3d1a1afa80e0c2896a16e
Tobias Kunze <rixx@cutebit.de>
2016-12-02 13:40:38 +0100

    Don't use the admin

--- src/cottidie/settings.py
+++ src/cottidie/settings.py
@@ -24,7 +24,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 DJANGO_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
