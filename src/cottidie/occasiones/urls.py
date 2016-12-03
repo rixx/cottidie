260987bce0e419bb922efb6d2d4aafc6c0dc6d79
Tobias Kunze <rixx@cutebit.de>
2016-12-03 15:12:11 +0100

    More reasonable url schema for occasiones

--- src/cottidie/occasiones/urls.py
+++ src/cottidie/occasiones/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url('^occasio/(?P<pk>\d+)/new/$', views.OccasionCreateView.as_view(), name='occasion-create'),
-    url('^occasio/(?P<pk>\d+)/$', views.OccasionView.as_view(), name='occasion-type'),
-    url('^occasio/new/$', views.OccasionTypeCreateView.as_view(), name='occasion-type-create'),
-    url('^occasio/$', views.OccasionTypeView.as_view(), name='occasion-type-list'),
+    url('^(?P<pk>\d+)/new/$', views.OccasionCreateView.as_view(), name='occasion-create'),
+    url('^(?P<pk>\d+)/$', views.OccasionView.as_view(), name='occasion-type'),
+    url('^list/$', views.OccasionTypeView.as_view(), name='occasion-type-list'),
+    url('^new/$', views.OccasionTypeCreateView.as_view(), name='occasion-type-create'),
     url('^$', views.MainView.as_view(), name='main'),
 ]
